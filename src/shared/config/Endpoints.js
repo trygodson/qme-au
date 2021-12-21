7091d2d6ec4417568cb764b7a1dd25ae157578f8
trygodson <gnqnpn@gmail.com>
2021-12-21 08:27:20 +0100

    editted end point

--- src/shared/config/Endpoints.js
+++ src/shared/config/Endpoints.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:8000/api/v1/';
+const BASE_URL = 'http://shop.scnip.net/api/v1/';
+// const BASE_URL = 'http://localhost:8000/api/v1/';
 
 const ApiEndpoints = {
   // Users
